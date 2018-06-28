2523541e43aeb8713b87da1e520148eb16ea7d27
clkeisel <clkeisel@yahoo.com>
2018-06-28 13:51:14 -0600

    First Commit of todo RESTful API

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var mongoose = require('mongoose');
+var Task = require('./api/models/todoListModel');
+var bodyParser = require('body-parser');
+
+var app = express()
+var port = process.env.port || 3000;
+
+mongoose.connect('mongodb://clkeisel:Eagles52champs!@ds121331.mlab.com:21331/true_solutions_todo_list')
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found.'});
+});
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
